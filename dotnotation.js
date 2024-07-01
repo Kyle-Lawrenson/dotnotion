3a4d994bfc1e868d7b1b8a24f94a621b6990648f
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-01 15:03:56 +0100

    add another example

--- dotnotation.js
+++ dotnotation.js
@@ -3,18 +3,19 @@
 
 // console.log (Math.floor (Math.random () *10) )
 
-console.log ("   |   |   ")
-console.log ("   |   |   ")
-console.log ("   |   |   ")
-console.log ("-----------")
-console.log ("   |   |   ")
-console.log ("   |   |   ")
-console.log ("   |   |   ")
-console.log ("-----------")
-console.log ("   |   |   ")
-console.log ("   |   |   ")
-console.log ("   |   |   ")
+// console.log ("   |   |   ")
+// console.log ("   |   |   ")
+// console.log ("   |   |   ")
+// console.log ("-----------")
+// console.log ("   |   |   ")
+// console.log ("   |   |   ")
+// console.log ("   |   |   ")
+// console.log ("-----------")
+// console.log ("   |   |   ")
+// console.log ("   |   |   ")
+// console.log ("   |   |   ")
 
+console.log ("All Around the World" .charAt (7) .toUpperCase )
 
 
 
