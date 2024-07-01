b7acbf4586bc9d89f32218fa50d35f1e82c9ee16
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-01 12:52:25 +0100

    initial commit

---
+++ dotnotation.js
@@ -0,0 +1,22 @@
+// console.log("Hello world".toUpperCase() )
+// console.log("Hello world".toLowerCase() )
+
+// console.log (Math.floor (Math.random () *10) )
+
+console.log ("   |   |   ")
+console.log ("   |   |   ")
+console.log ("   |   |   ")
+console.log ("-----------")
+console.log ("   |   |   ")
+console.log ("   |   |   ")
+console.log ("   |   |   ")
+console.log ("-----------")
+console.log ("   |   |   ")
+console.log ("   |   |   ")
+console.log ("   |   |   ")
+
+
+
+
+
+
