88811773af470017d6b42c647668d7a7516b070c
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-01 18:58:50 +0100

    add 2nd activity

--- activities.js
+++ activities.js
@@ -4,7 +4,6 @@
 
 // console.log("Hey my name is " +firstName+ ". I am currently " +age+ ". My favourite colour is " +favouriteColour+ ". ")
 
-// console.log('Hey, my name is ${firstName}. I am ${age} years old and my fsvourite colour is ${favouriteColour}.')
 
 // let firstName= "bob" ;
 // let age= "20" ;
@@ -12,8 +11,8 @@
 
 // console.log("Hey my name is " +firstName+ ". I am currently " +age+ ". My favourite colour is " +favouriteColour+ ". ")
 
-let breakfast= "toast"
-let lunch= "a ham sandwich"
-let dinner= "pizza"
+// let breakfast= "toast"
+// let lunch= "a ham sandwich"
+// let dinner= "pizza"
 
-console.log("This morning for breakfast I had " +breakfast+ ". For lunch I ate " +lunch+ ". For dinner i will eat " +dinner+ ". ")
+// console.log("This morning for breakfast I had " +breakfast+ ". For lunch I ate " +lunch+ ". For dinner i will eat " +dinner+ ". ")
