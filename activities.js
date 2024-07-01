650b27dd54b9366190aaf1548b6105830fa6b467
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-01 15:57:26 +0100

    start activity

---
+++ activities.js
@@ -0,0 +1,19 @@
+// let firstName= "kyle" ;
+// let age= "24" ;
+// let favouriteColour= "turquoise" ;
+
+// console.log("Hey my name is " +firstName+ ". I am currently " +age+ ". My favourite colour is " +favouriteColour+ ". ")
+
+// console.log('Hey, my name is ${firstName}. I am ${age} years old and my fsvourite colour is ${favouriteColour}.')
+
+// let firstName= "bob" ;
+// let age= "20" ;
+// let favouriteColour= "green" ;
+
+// console.log("Hey my name is " +firstName+ ". I am currently " +age+ ". My favourite colour is " +favouriteColour+ ". ")
+
+let breakfast= "toast"
+let lunch= "a ham sandwich"
+let dinner= "pizza"
+
+console.log("This morning for breakfast I had " +breakfast+ ". For lunch I ate " +lunch+ ". For dinner i will eat " +dinner+ ". ")
